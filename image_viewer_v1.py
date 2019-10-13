8943dc6e9b3ce5f4467035fd61af09b2f0672cbc
gabri <gabriel.garcia..hernando@gmail.com>
2019-10-13 15:34:58 +0200

    working in viewer and imagelist

--- image_viewer_v1.py
+++ image_viewer_v1.py
@@ -38,9 +38,11 @@ if __name__ == '__main__':
     for row in range(nrows):
         for col in range(ncols):
             images[n].render(ax[row, col])
+            ax[row,col].title.set_text(images[n].title)
             n = n+1
             if n >= len(images):
                 break
+    plt.axis('off')
     plt.show()
 
 
